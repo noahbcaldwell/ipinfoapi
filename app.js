b38becaf09254ae6aa827272369d963587b7df1f
noahbcaldwell <107315487+noahbcaldwell@users.noreply.github.com>
2023-12-01 10:26:51 -0600

    Port 80

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 80;
 
 app.get('/geolocation/:ip', async (req, res) => {
   try {
