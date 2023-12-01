0a7c232d6de83627d6ac404d6895d994adc47726
noahbcaldwell <107315487+noahbcaldwell@users.noreply.github.com>
2023-12-01 10:17:08 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/geolocation/:ip', async (req, res) => {
+  try {
+    const ip = req.params.ip;
+    const response = await axios.get(`https://ipinfo.io/${ip}/json`);
+    const geolocationData = response.data;
+
+    res.json({
+      ip: geolocationData.ip,
+      city: geolocationData.city,
+      region: geolocationData.region,
+      country: geolocationData.country,
+      loc: geolocationData.loc,
+      org: geolocationData.org,
+    });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
